5f9a337512c6964885d2dd51d795447b8ce9e264
Kévin Albespy <kevin.albespy@gmail.com>
2023-01-31 23:08:51 +0100

    Add vote and collection relation between Gif and user

--- src/Entity/Gif.php
+++ src/Entity/Gif.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GifRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GifRepository::class)]
@@ -21,6 +23,18 @@ class Gif
 
     #[ORM\Column(nullable: true)]
     private ?int $nbOfVotes = null;
+
+    #[ORM\ManyToMany(targetEntity: User::class, mappedBy: 'vote')]
+    private Collection $voters;
+
+    #[ORM\ManyToMany(targetEntity: User::class, mappedBy: 'collection')]
+    private Collection $collectors;
+
+    public function __construct()
+    {
+        $this->voters = new ArrayCollection();
+        $this->collectors = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -59,6 +73,60 @@ class Gif
     public function setNbOfVotes(?int $nbOfVotes): self
     {
         $this->nbOfVotes = $nbOfVotes;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getVoters(): Collection
+    {
+        return $this->voters;
+    }
+
+    public function addVoter(User $voter): self
+    {
+        if (!$this->voters->contains($voter)) {
+            $this->voters->add($voter);
+            $voter->addToVotes($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVoter(User $voter): self
+    {
+        if ($this->voters->removeElement($voter)) {
+            $voter->removeFromVotes($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getCollectors(): Collection
+    {
+        return $this->collectors;
+    }
+
+    public function addCollector(User $collector): self
+    {
+        if (!$this->collectors->contains($collector)) {
+            $this->collectors->add($collector);
+            $collector->addToCollection($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCollector(User $collector): self
+    {
+        if ($this->collectors->removeElement($collector)) {
+            $collector->removeFromCollection($this);
+        }
 
         return $this;
     }
